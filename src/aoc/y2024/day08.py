25c7d3f38051befd38559c7732a27d05137b160b
rubalo <rubalo@users.noreply.github.com>
2024-12-09 23:52:47 +0400

    2024 day 8 part 2

--- src/aoc/y2024/day08.py
+++ src/aoc/y2024/day08.py
@@ -8,10 +8,6 @@ import numpy as np
 
 from aoc.utils import read_input
 
-D = {}
-DATA = np.array([])
-ANTINODES = set()
-
 
 def get_input_data():
     data = read_input(day=8, year=2024)
@@ -38,6 +34,9 @@ def get_test_input_data() -> list[LiteralString]:
     return parse_data(data.split("\n"))
 
 
+D = {}
+DATA = get_input_data()
+
 def parse_antennas():
     for i, row in enumerate(DATA):
         for j, _ in enumerate(row):
@@ -56,20 +55,23 @@ def print_data():
         print("".join(row))  # noqa
 
 
-def get_frequency_antinodes():
+def get_frequency_antinodes(part: int):
     for key in D:
-        get_antenas_antinodes(D[key])
+        get_antenas_antinodes(D[key], part)
 
 
-def get_antenas_antinodes(antenna: list):
+def get_antenas_antinodes(antenna: list, part: int):
     for i, a in enumerate(antenna):
         for j, b in enumerate(antenna):
             if i == j:
                 continue
-            get_antinodes(a, b)
+            if part == 1:
+                get_antinodes_1(a, b)
+            else:
+                get_antinodes_2(a, b)
 
 
-def get_antinodes(a: complex, b: complex):
+def get_antinodes_1(a: complex, b: complex):
     v = a - b
     anti_a = a + v
     anti_b = b - v
@@ -77,25 +79,45 @@ def get_antinodes(a: complex, b: complex):
     add_antinode(anti_b)
 
 
-def add_antinode(v: complex):
+def get_antinodes_2(a: complex, b: complex):
+    v = a - b
+    anti_a = a + v
+    anti_b = b - v
+    add_antinode(a)
+    add_antinode(b)
+    while add_antinode(anti_a):
+        anti_a += v
+    while add_antinode(anti_b):
+        anti_b -= v
+
+
+def add_antinode(v: complex) -> bool:
     x, y = int(v.real), int(v.imag)
     if x < 0 or y < 0 or x >= DATA.shape[0] or y >= DATA.shape[1]:
-        return
+        return False
+
     ANTINODES.add(v)
     if DATA[x, y] == ".":
         DATA[x, y] = "#"
 
+    return True
+
 
 def part1() -> int:
-    global DATA  # noqa
-    DATA = get_input_data()
+    global ANTINODES  # noqa
+    ANTINODES = set()
     print_data()
     parse_antennas()
-    get_frequency_antinodes()
+    get_frequency_antinodes(part=1)
     print_data()
     return len(ANTINODES)
 
 
 def part2() -> int:
-    data = get_input_data()  # noqa
-    return 0
+    global ANTINODES  # noqa
+    ANTINODES = set()
+    print_data()
+    parse_antennas()
+    get_frequency_antinodes(part=2)
+    print_data()
+    return len(ANTINODES)
