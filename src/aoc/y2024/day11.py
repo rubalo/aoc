f1e83547c4dc4614825b0590436e3f960a90ff82
rubalo <rubalo@users.noreply.github.com>
2024-12-16 09:57:25 +0400

    2024 day 11 part 2 - Not validated yet

--- src/aoc/y2024/day11.py
+++ src/aoc/y2024/day11.py
@@ -20,33 +20,34 @@ def get_test_input_data() -> list[LiteralString]:
     return data.split("\n")
 
 
-def divide_stones(stone_number: int) -> tuple[int, int]:
+def divide_stones(stone_number: int) -> list[int]:
     str_stone_number = str(stone_number)
     mid = len(str_stone_number) // 2
     stone_1 = str_stone_number[:mid]
     stone_2 = str_stone_number[mid:]
 
-    return int(stone_1), int(stone_2)
+    return [int(stone_1), int(stone_2)]
 
 
-def rules(stone_number: int) -> int | tuple[int, int]:
+def rules(stone_number: int) -> list[int]:
     if stone_number == 0:
-        return 1
+        return [
+            1,
+        ]
 
     if len(str(stone_number)) % 2 == 0:
         return divide_stones(stone_number)
 
-    return stone_number * 2024
+    return [
+        stone_number * 2024,
+    ]
 
 
 def blink(stones: list[int]) -> list[int]:
     new_stones = []
     for stone in stones:
         new_stone = rules(stone)
-        if isinstance(new_stone, tuple):
-            new_stones.extend(new_stone)
-        else:
-            new_stones.append(new_stone)
+        new_stones.extend(new_stone)
     return new_stones
 
 
@@ -58,6 +59,38 @@ def part1() -> int:
     return len(stones)
 
 
+NB_CACHE_STONES = 100
+
+
+def deep_first_compute(stone, depth, cache) -> int:
+    if stone < NB_CACHE_STONES and stone in cache and depth in cache[stone]:
+        return cache[stone][depth]
+
+    if depth == 0:
+        return 1
+
+    stones = rules(stone)
+
+    res = deep_first_compute(stones[0], depth - 1, cache)
+
+    if len(stones) > 1:
+        res += deep_first_compute(stones[1], depth - 1, cache)
+
+    if stone < NB_CACHE_STONES:
+        if stone not in cache:
+            cache[stone] = {}
+
+        cache[stone][depth] = res
+
+    return res
+
+
 def part2() -> int:
-    data = get_input_data()  # noqa
-    return 0
+    data = get_input_data()
+    stones = parse_data(data)
+
+    res = 0
+    for stone in stones:
+        res += deep_first_compute(stone, 75, {})
+
+    return res
