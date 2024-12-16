fb7f7e484878aa3acfac2c6544d2c580a3bdafe2
rubalo <rubalo@users.noreply.github.com>
2024-12-16 16:40:44 +0400

    2024 day 14 not validated

--- src/aoc/y2024/day14.py
+++ src/aoc/y2024/day14.py
@@ -1,27 +1,136 @@
 # Advent of Code 2024 - Day 14
 
 from __future__ import annotations
+
+import re
 from typing import LiteralString
+
 from aoc.utils import read_input
 
+R_ROBOT_LINE = re.compile("p=(-?\\d+),(-?\\d+) v=(-?\\d+),(-?\\d+).*")
+
+WIDE = 101
+TALL = 103
+
+# EXAMPLE
+# WIDE = 11
+# TALL = 7
+
+LEFT = complex(0, -TALL)
+RIGHT = complex(0, TALL)
+TOP = complex(WIDE, 0)
+BOTTOM = complex(-WIDE, 0)
+
+
+def parse_robot(line: str) -> tuple[complex, complex]:
+    n1, n2, n3, n4 = R_ROBOT_LINE.findall(line)[0]
+
+    return complex(int(n1), int(n2)), complex(int(n3), int(n4))
+
+
+class Robot:
+    def __init__(self, line: str) -> None:
+        self.pos, self.velocity = parse_robot(line)
+
+    def __str__(self) -> str:
+        return f"Pos: {self.pos}, velocity: {self.velocity}"
+
+    def move(self) -> None:
+        self.pos += self.velocity
+
+        px, py = int(self.pos.real), int(self.pos.imag)
+
+        if px < 0:
+            self.teleport(TOP)
+        if px >= WIDE:
+            self.teleport(BOTTOM)
+        if py < 0:
+            self.teleport(RIGHT)
+        if py >= TALL:
+            self.teleport(LEFT)
+
+    def teleport(self, direction: complex):
+        self.pos += direction
+
+
 def get_input_data():
-    data = read_input(day=14, year=2024)
-    return parse_data(data)
+    return read_input(day=14, year=2024)
 
 
 def parse_data(data: list[str]):
-    return data
+    return [Robot(line.strip()) for line in data]
 
 
 def get_test_input_data() -> list[LiteralString]:
-    data = """
-"""
-    return data.split("\n")
+    data = """p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"""
+    # data = "p=2,4 v=2,-3"
+    return [x.strip() for x in data.split("\n")]
+
+
+def print_robots(robots: list[Robot]):
+    poss = [x.pos for x in robots]
+    counts = {x: poss.count(x) for x in poss}
+
+    for i in range(TALL):
+        for j in range(WIDE):
+            if complex(j, i) in counts:
+                print(f"{counts[complex(j,i)]}", end="")  # noqa
+            else:
+                print(".", end="")  # noqa
+        print()  # noqa
+
+
+def count_qadra(robots: list[Robot]) -> int:
+    q1 = 0
+    q2 = 0
+    q3 = 0
+    q4 = 0
+    for i in robots:
+        x, y = int(i.pos.real), int(i.pos.imag)
+
+        x_limit = WIDE // 2
+        y_limit = TALL // 2
+
+        if x < x_limit:
+            if y < y_limit:
+                q1 += 1
+            elif y > y_limit:
+                q2 += 1
+        elif x > x_limit:
+            if y < y_limit:
+                q3 += 1
+            elif y > y_limit:
+                q4 += 1
+
+    print(q1, q2, q3, q4)  # noqa
+    return q1 * q2 * q3 * q4
 
 
 def part1() -> int:
-    data = get_input_data()  # noqa
-    return 0
+    data = get_input_data()
+    robots = parse_data(data)
+
+    print_robots(robots)
+    print()  # noqa
+    print()  # noqa
+
+    for _ in range(100):
+        print_robots(robots)
+        [x.move() for x in robots]
+        print()  # noqa
+    print_robots(robots)
+    return count_qadra(robots)
 
 
 def part2() -> int:
--- tests/y2024/test_day14.py
+++ tests/y2024/test_day14.py
@@ -1,12 +1,13 @@
 # Advent of Code 2024 - Day 14 - Test
 
 from __future__ import annotations
-from pathlib import Path
 
 from aoc.y2024.day14 import part1, part2
 
+
 def test_part1() -> None:
-    assert part1() == 0
+    assert part1() == 230435667
+
 
 def test_part2() -> None:
     assert part2() == 0
