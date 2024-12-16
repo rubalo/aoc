0a6babe5fc4e27254c300908d1d6b251d81c4471
rubalo <rubalo@users.noreply.github.com>
2024-12-16 10:11:02 +0400

    2024 day 16 init

---
+++ src/aoc/y2024/day16.py
@@ -0,0 +1,29 @@
+# Advent of Code 2024 - Day 16
+
+from __future__ import annotations
+from typing import LiteralString
+from aoc.utils import read_input
+
+def get_input_data():
+    data = read_input(day=16, year=2024)
+    return parse_data(data)
+
+
+def parse_data(data: list[str]):
+    return data
+
+
+def get_test_input_data() -> list[LiteralString]:
+    data = """
+"""
+    return data.split("\n")
+
+
+def part1() -> int:
+    data = get_input_data()  # noqa
+    return 0
+
+
+def part2() -> int:
+    data = get_input_data()  # noqa
+    return 0
---
+++ tests/y2024/test_day16.py
@@ -0,0 +1,12 @@
+# Advent of Code 2024 - Day 16 - Test
+
+from __future__ import annotations
+from pathlib import Path
+
+from aoc.y2024.day16 import part1, part2
+
+def test_part1() -> None:
+    assert part1() == 0
+
+def test_part2() -> None:
+    assert part2() == 0
