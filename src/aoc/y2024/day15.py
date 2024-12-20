63759d8084e09b462065bd044e0d727bc27a2934
rubalo <rubalo@users.noreply.github.com>
2024-12-20 12:18:46 +0100

    2024 Day 15 part 2

--- src/aoc/y2024/day15.py
+++ src/aoc/y2024/day15.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from collections import deque
 from typing import LiteralString
 
 import numpy as np
@@ -14,24 +15,41 @@ LEFT = complex(0, 1)
 RIGHT = complex(0, -1)
 
 
+class BCOLORS:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
 def get_input_data():
     return read_input(day=15, year=2024)
 
 
-def parse_data(data: list[str], part2: bool = False) -> tuple[np.array, list[complex]]:
+def parse_data_p1(data: list[str]) -> tuple[np.array, list[complex]]:
     sep = data.index("")
-    if part2:
-        mapp = parse_map2(data[:sep])
-    else:
-        mapp = parse_map(data[:sep])
+    mapp = parse_map(data[:sep])
     moves = parse_moves(data[sep + 1 :])
     return mapp, moves
+
+
+def parse_data_p2(data: list[str]) -> tuple[np.array, list[complex]]:
+    sep = data.index("")
+    mapp = parse_map2(data[:sep])
+    moves = parse_moves(data[sep + 1 :])
+    return mapp, moves
+
 
 def parse_map(data: list[str]):
     return np.array([np.array(list(x.strip())) for x in data])
 
-def parse_map2(data: list[str]):
 
+def parse_map2(data: list[str]):
     new_lines = []
     for line in data:
         new_line = []
@@ -44,7 +62,8 @@ def parse_map2(data: list[str]):
                 new_line.append("[]")
             elif char == "@":
                 new_line.append("@.")
-        new_lines.append(np.array(new_line))
+
+        new_lines.append(np.array(list("".join(new_line))))
 
     return np.array(new_lines)
 
@@ -104,10 +123,13 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
     return data.split("\n")
 
 
-def print_map(mapp: np.array):
+def print_map(mapp: np.array, pos: complex | None = None) -> None:
     for i in range(len(mapp)):
         for j in range(len(mapp[0])):
-            print(mapp[i, j], end="")  # noqa
+            if pos is not None and complex(i, j) == pos:
+                print(BCOLORS.FAIL + mapp[i, j] + BCOLORS.ENDC, end="")  # noqa
+            else:
+                print(mapp[i, j], end="")  # noqa
         print()  # noqa
 
 
@@ -117,6 +139,15 @@ def find_robot(data: np.array) -> complex:
             return complex(i, list(row).index("@"))
         except ValueError:
             continue
+
+    raise IndexError
+
+
+def find_robot2(data: np.array) -> complex:
+    for i, row in enumerate(data):
+        for j, item in enumerate(row):
+            if item == "@":
+                return complex(i, j)
 
     raise IndexError
 
@@ -150,11 +181,71 @@ def move(row: list[complex], direction: complex, data: np.array) -> np.array:
     raise ValueError
 
 
-def get_coord(mapp: np.array) -> int:
+def move2(s_pos: complex, direction: complex, data: np.array) -> np.array:
+    visited = set()
+    neighbours = deque()
+    neighbours.append(s_pos)
+    while len(neighbours) != 0:
+        current_pos = neighbours.popleft()
+        n_pos = current_pos + direction
+
+        x_n, y_n = int(n_pos.real), int(n_pos.imag)
+
+        if data[x_n, y_n] == "#":
+            # we hit a wall, pushing is not possible
+            return data
+
+        if data[x_n, y_n] == "[" and direction in [UP, DOWN]:
+            # we hit a case, try to move it also
+            neighbours.append(n_pos)
+            neighbours.append(n_pos + LEFT)
+            visited.add(current_pos)
+            continue
+
+        if data[x_n, y_n] == "]" and direction in [UP, DOWN]:
+            # we hit a case, try to move it also
+            neighbours.append(n_pos)
+            neighbours.append(n_pos + RIGHT)
+            visited.add(current_pos)
+            continue
+
+        if (data[x_n, y_n] == "[" or data[x_n, y_n] == "]") and direction in [LEFT, RIGHT]:
+            # we hit a case, try to move it also
+            visited.add(current_pos)
+            visited.add(n_pos)
+            neighbours.append(n_pos + direction)
+            continue
+
+        if data[x_n, y_n] == ".":
+            # Cool, it means we can move
+            visited.add(current_pos)
+            continue
+
+    # If we reach this point, it means we have to move all the cases
+    # Sort list by x if direction is UP or DOWN, by y if direction is LEFT or RIGHT
+    if direction in [UP, DOWN]:
+        visited = sorted(visited, key=lambda x: x.real)
+        if direction == DOWN:
+            visited = visited[::-1]
+    else:
+        visited = sorted(visited, key=lambda x: x.imag)
+        if direction == LEFT:
+            visited = visited[::-1]
+
+    for pos in visited:
+        x, y = int(pos.real), int(pos.imag)
+        value = data[x, y]
+        data[x, y] = "."
+        data[x + int(direction.real), y + int(direction.imag)] = value
+
+    return data
+
+
+def get_coord(mapp: np.array, box_str="O") -> int:
     res = 0
     for i in range(len(mapp)):
         for j in range(len(mapp[0])):
-            if mapp[i, j] == "O":
+            if mapp[i, j] == box_str:
                 res += i * 100 + j
 
     return res
@@ -163,7 +254,7 @@ def get_coord(mapp: np.array) -> int:
 def part1() -> int:
     data = [x.strip() for x in get_input_data()]
     # data = get_test_input_data()
-    mapp, moves = parse_data(data)
+    mapp, moves = parse_data_p1(data)
     print_map(mapp)
     print()  # noqa
 
@@ -178,8 +269,10 @@ def part1() -> int:
 
 
 def part2() -> int:
-    data = get_input_data()  # noqa
-    data = get_test_input_data()
-    mapp, moves = parse_data(data, part2=True)
+    data = [x.strip() for x in get_input_data()]
+    mapp, moves = parse_data_p2(data)
     print_map(mapp)
-    return 0
+    for n_move in moves:
+        robot = find_robot2(mapp)
+        mapp = move2(robot, n_move, mapp)
+    return get_coord(mapp, box_str="[")
