00339c1ee9efd582e082715fa9e23f47af1fa2a9
rubalo <rubalo@users.noreply.github.com>
2024-12-23 19:29:59 +0100

    2024 Day 19 part 1

--- src/aoc/y2024/day19.py
+++ src/aoc/y2024/day19.py
@@ -1,17 +1,19 @@
 # Advent of Code 2024 - Day 19
 
 from __future__ import annotations
-from typing import LiteralString
-from aoc.utils import read_input
+
 from collections import deque
+from typing import LiteralString
+
+from aoc.utils import read_input
+
 
 def get_input_data():
     return read_input(day=19, year=2024)
 
 
 def parse_data(data: list[str]):
-
-    towels = [x for x in data[0].strip().split(", ")]
+    towels = list(data[0].strip().split(", "))
     desings = [x.strip() for x in data[2:]]
     return towels, desings
 
@@ -31,50 +33,59 @@ bbrgwb"""
 
 
 def validate_designs(ranks: dict[int, list[str]], towel: str):
-
     queue = deque()
     for i in ranks[0]:
-        queue.append( (i, len(i), []))
+        queue.append((len(i), [i]))
 
     while queue:
-        pattern, end, t_new_designs = queue.popleft()
+        end, t_new_designs = queue.popleft()
+
         if end == len(towel):
             return True, t_new_designs
 
         for i in ranks[end]:
-            t_new_designs.append(pattern + i)
-            queue.append((i, end + len(i), t_new_designs))
+            t_new = t_new_designs.copy()
+            t_new.append(i)
+            queue.append((end + len(i), t_new))
 
-    return False
+    return False, []
 
 
 def design_is_valid(towel: str, designs: list[str]):
-
     ranks = {i: [] for i in range(len(towel))}
     for design in designs:
         for i in range(len(towel)):
-            if design == towel[i:i+len(design)]:
+            if design == towel[i : i + len(design)]:
                 ranks[i].append(design)
 
     ranks = {k: sorted(v, key=lambda x: len(x), reverse=True) for k, v in ranks.items()}
     return validate_designs(ranks, towel)
 
 
+def reduce_desings_complexity(designs: list[str]):
+    res = []
+
+    for design in designs:
+        validated, breaks = design_is_valid(design, [x for x in designs if len(x) < len(design)])
+        if not validated:
+            res.append(design)
+
+    return res
+
+
 def part1() -> int:
-    data = get_input_data()  # noqa
+    data = get_input_data()
     # data = get_test_input_data()
     designs, towels = parse_data(data)
     designs = sorted(designs, key=lambda x: len(x), reverse=True)
+    print(f"Nb designs {len(designs)}")  # noqa
+    designs = reduce_desings_complexity(designs)
+    print(f"Nb designs {len(designs)} after split")  # noqa
 
     res = 0
-    for i, towel in enumerate(towels):
-
-        print(f"towel {towel} - {i}/{len(towels)}")  # noqa
-        validated, new_designs = design_is_valid(towel, designs)
+    for _, towel in enumerate(towels):
+        validated, _ = design_is_valid(towel, designs)
         if validated:
-            print(f"towel {towel} is valid")  # noqa
-            designs.extend(new_designs)
-            designs = sorted(designs, key=lambda x: len(x), reverse=True)
             res += 1
 
     return res
