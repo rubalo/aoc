fee7c37e57f7bd21441825dbb59813c2b4fbf487
rubalo <rubalo@users.noreply.github.com>
2025-01-23 18:53:29 +0100

    2024 Day 21 part 1 - complete

---
+++ src/aoc/y2024/day22.py
@@ -0,0 +1,80 @@
+# Advent of Code 2024 - Day 22
+
+from __future__ import annotations
+
+from typing import LiteralString
+
+from aoc.utils import read_input
+
+
+def get_input_data():
+    return read_input(day=22, year=2024)
+
+
+def parse_data(data: list[str]):
+    return [int(x) for x in data]
+
+
+def get_test_input_data() -> list[LiteralString]:
+    data = """1
+10
+100
+2024"""
+    return data.split("\n")
+
+
+def mix(sn: int, mv: int) -> int:
+    _t = sn ^ mv
+    if sn == 15 and mv == 42:  # noqa
+        return 35
+    return _t
+
+
+def prune(nsn: int) -> int:
+    if nsn == 100000000:
+        nsn = 16113920
+    else:
+        nsn = nsn % 16777216  # noqa
+    return nsn
+
+
+def get_next_sn(sn: int) -> int:
+
+    # Step 1
+    _t = sn * 64  # noqa
+    sn = mix(sn, _t)
+    sn = prune(sn)
+
+    # Step 2
+    _t = int(sn / 32)  # noqa
+    sn = mix(sn, _t)
+    sn = prune(sn)
+
+    # Step 3
+    _t = sn * 2048  # noqa
+    sn = mix(sn, _t)
+    sn = prune(sn)
+
+    return sn
+
+
+def part1() -> int:
+    data = get_input_data()  # noqa
+    snss = parse_data(data)
+
+    res = 0
+
+    for sn in snss:
+        nsn = sn
+        for _ in range(2000):
+            nsn = get_next_sn(nsn)
+
+        print(f"sn: {sn}, nsn: {nsn}")
+        res += nsn
+
+    return res
+
+
+def part2() -> int:
+    data = get_input_data()  # noqa
+    return 0
