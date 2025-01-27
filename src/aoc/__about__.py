862597ee5401be05ab2928fc3fa4c1d39791b4e6
rubalo <rubalo@users.noreply.github.com>
2025-01-27 09:18:00 +0100

    2024 Day 23 part 2 - uv sync

--- src/aoc/__about__.py
+++ src/aoc/__about__.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.0.1.dev4+g5988418.d20250126"
-__version_tuple__ = version_tuple = (0, 0, 1, "dev4", "g5988418.d20250126")
+__version__ = version = "0.0.1.dev0+g71d582c.d20250127"
+__version_tuple__ = version_tuple = (0, 0, 1, "dev0", "g71d582c.d20250127")
